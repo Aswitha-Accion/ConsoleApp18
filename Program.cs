80fb0ec2dd346bbd9ea9105abd423b66a6b0c41b
Aswitha-Accion <aswitha.pathireddy@accionlabs.com>
2023-08-16 17:52:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace Application;
+public class Program
+{
+    public void CreateTable()
+    {
+        SqlConnection con = null;
+        try
+        {
+            con = new SqlConnection("data source = .; database = Employee; integrated security = SSPI");
+
+            SqlCommand cm = new SqlCommand("create table Employee(id int not null, name varchar(50),  email varchar(50), city varchar(50)", con);
+
+            con.Open();
+            cm.ExecuteNonQuery();
+
+            Console.WriteLine("Table created successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("something went wrong" + ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+
+
+    }
+}
+
+class Program1
+{
+    public static void Main()
+    {
+        new Program().CreateTable();
+        Console.ReadKey();
+    }
+}
+
